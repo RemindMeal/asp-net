4787294effff9cffc9a148507477a7526517a3f5
Edouard Berthe <berthe.ed@gmail.com>
2020-04-11 17:15:14 +0200

    Improves Login.cshtml.cs

--- src/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -69,13 +69,14 @@ namespace RemindMeal.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl ??= Url.Content("~/");
 
             if (ModelState.IsValid)
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("Vous êtes bien connecté(e).");
