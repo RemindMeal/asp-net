37949feb5b0b749fc0fcd89329eb7932f81d1eb7
Edouard Berthe <berthe.ed@gmail.com>
2020-11-29 22:54:42 +0100

    Add recipes on Friend page

--- src/Pages/Recipes/Details.cshtml.cs
+++ src/Pages/Recipes/Details.cshtml.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,6 +19,13 @@ namespace RemindMeal.Pages.Recipes
         }
 
         public Recipe Recipe { get; set; }
+        
+        public IEnumerable<(Friend, int)> Friends =>
+            Recipe
+                .Cookings
+                .SelectMany(cooking => cooking.Meal.Presences.Select(p => p.Friend))
+                .GroupBy(friend => friend)
+                .Select(g => (g.Key, g.Count()));
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
