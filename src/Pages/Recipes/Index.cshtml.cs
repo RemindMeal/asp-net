570927a6488a4b70a2bb65b0fef9f683954cdc96
Edouard Berthe <berthe.ed@gmail.com>
2020-12-01 00:34:11 +0100

    Add sort to Recipe Index page

--- src/Pages/Recipes/Index.cshtml.cs
+++ src/Pages/Recipes/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,21 +21,73 @@ namespace RemindMeal.Pages.Recipes
 
         public IList<Recipe> Recipes { get; set; }
 
-        public async Task OnGetAsync()
+        // Sort
+        public SortOrder NameSort { get; set; }
+        public string NameSortLink => Convert(Invert(NameSort));
+
+        // Search
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public async Task OnGetAsync(string nameSortOrder)
         {
             var recipes = from m in _context.Recipes select m;
+            
             if (!string.IsNullOrEmpty(SearchString))
             {
                 var lowerSearchString = SearchString.ToLower();
-                recipes = recipes.Where(r => r.Name.ToLower().Contains(lowerSearchString) || r.Description.ToLower().Contains(lowerSearchString));
+                recipes = recipes.Where(r => 
+                    r.Name.ToLower().Contains(lowerSearchString) ||
+                    r.Description.ToLower().Contains(lowerSearchString));
             }
+
+            NameSort = Convert(nameSortOrder);
+
+            recipes = NameSort == SortOrder.Descending
+                ? recipes.OrderByDescending(r => r.Name)
+                : recipes.OrderBy(r => r.Name);
+
             Recipes = await recipes
                 .Include(r => r.RecipeTags)
                 .ThenInclude(rt => rt.Tag)
                 .ToListAsync();
         }
 
-        [BindProperty(SupportsGet = true)]
-        public string SearchString { get; set; }
+        public enum SortOrder
+        {
+            Ascending,
+            Descending
+        }
+        
+        private static SortOrder Invert(SortOrder order)
+        {
+            switch (order)
+            {
+                case SortOrder.Ascending:
+                    return SortOrder.Descending;
+                case SortOrder.Descending:
+                    return SortOrder.Ascending;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(order), order, null);
+            }
+        }
+
+        private static SortOrder Convert(string sortOrderString)
+        {
+            return sortOrderString == "desc" ? SortOrder.Descending : SortOrder.Ascending;
+        }
+
+        private static string Convert(SortOrder sortOrder)
+        {
+            switch (sortOrder)
+            {
+                case SortOrder.Ascending:
+                    return "asc";
+                case SortOrder.Descending:
+                    return "desc";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(sortOrder), sortOrder, null);
+            }
+        }
     }
 }
