7402d676493260202ef44658ee5ed19e1f8dc9a4
Edouard Berthe <berthe.ed@gmail.com>
2021-01-02 08:58:16 +0100

    Automatically migrate DB on Startup

--- src/Startup.cs
+++ src/Startup.cs
@@ -87,8 +87,9 @@ namespace RemindMeal
             }
             else
             {
+                var db = app.ApplicationServices.GetService<DbContext>();
+                db.Database.Migrate();
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
