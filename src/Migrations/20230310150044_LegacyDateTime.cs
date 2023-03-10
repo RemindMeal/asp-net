6b9b41281d2580fc46d0c67c53eb2fe771b23238
Edouard Berthe <edouard.berthe@protonmail.com>
2023-03-10 15:05:51 +0000

    Add LegacyDateTime DB migration

---
+++ src/Migrations/20230310150044_LegacyDateTime.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RemindMeal.Migrations
+{
+    public partial class LegacyDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meals_AspNetUsers_UserId",
+                table: "Meals");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Meals",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Meals",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meals_AspNetUsers_UserId",
+                table: "Meals",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meals_AspNetUsers_UserId",
+                table: "Meals");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Meals",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Meals",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meals_AspNetUsers_UserId",
+                table: "Meals",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
